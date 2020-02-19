44e42309ebbf91cc61d595a08740c50e3441b2e4
Globe-Eater <bullokl@ostatemail.okstate.edu>
2020-02-19 10:06:02 -0600

    Added bivariate regression method, working on saving output to pdf.

--- Ex3/Correlations.py
+++ Ex3/Correlations.py
@@ -6,31 +6,78 @@ Created on Sun Feb 16 11:45:57 2020
 @author: Kellen Bullock
 """
 
-import math
 import pandas as pd
+import numpy as np
+from scipy import stats
+import statsmodels.api as sm
 import sys
 sys.path.append('/Users/kellenbullock/desktop/Geographic Analysis II/Ex1')
 import EDA
 
-
-def log_transformation(df):
+def log_trans(df, column):
     '''This method takes in a dataframe series and applys the log base 10
     transformation. 
     This method will print out a Shprio Wilks and Kolmogorov-Smirnov test 
     statstics.
     
     inputs:
-        pandas series or array.
+        df is the name of the DataFrame
+        column is the name of the column within the DataFrame to be used.  
+            column must be a string.
     
     returns:
         transformated pandas series or array
         2 print lines of normality tests
         
     Usage:
-        log_transformation(df)
+        log_transformation(Monsters['Rabbits'])
         
     '''
+    df = np.log10(df[column])
+    sm.qqplot(df, line='r')
+    print('Shaprio Wilks test: ', stats.shapiro(df))
+    print('Kolmogorov-Smirnov: ', stats.kstest(df, 'norm'))
+    print("")
+    return df
         
+def sqrt_trans(df, column):
+    ''' This method takes the input of a dataframe name and a column name.
+    This will return a square root transformation of the input series.
+    
+    inputs:
+        df = the name of the pandas Dataframe
+        column = the name of the series or column.
+        
+    returns:
+        A qq plot of result of the transformation.
+        2 print statements of Shaprio Wilks and Kolmogorov-Smirnov tests.
+        The newly transformed series.
+        
+    Usage:
+        Monsters['Rabbit'] = sqrt_trans(Monsters, 'Rabbit')'''
+    df = np.sqrt(df[column])
+    sm.qqplot(df, line='r')
+    print('Shaprio Wilks test: ', stats.shapiro(df))
+    print('Kolmogorov-Smirnov: ', stats.kstest(df, 'norm'))
+    print("")
+    return df
+
+def bivar_regres(independent, dependent):
+    '''This method is for doing bivartie regression. In other words it only regresses
+    two variables. 
+    
+    inputs:
+        indenpendent = the independent variable
+        
+        dependent = the dependent variable
+    
+    returns:
+        A statsmodels regression table to the console and
+        saves it out to a pdf file.'''
+    
+    mod = sm.OLS(independent, dependent)
+    res = mod.fit()
+    print(res.summary())
 
 #def main():
 state = pd.read_spss('/Users/kellenbullock/Desktop/Geographic Analysis II/Data/5303_EX_A.sav')
@@ -40,7 +87,8 @@ Schools = state.query("Scale == 'Schools'")
 Tracts = state.query("Scale == 'Tracts'")
 
 # Old varaibles:
-assigned_var_c = Counties[['Pct_Black', 'Pct_Two_Plus', 'Pct_SNAP', 'Pct_FIRE_I', 'Pct_Poverty']]
+assigned_var_c = Counties[['Pct_Black', 'Pct_Two_Plus', 'Pct_SNAP', 'Pct_FIRE_I', 'Pct_Poverty', 'Pct_Unemp', 'Med_HomeValue', 'Pct_White', 'Pct_BlueCollar_O', 'Pct_Hispanic']]
+assigned_c_repub = Counties[['Pct_Repub', 'Pct_Black', 'Pct_Two_Plus', 'Pct_SNAP', 'Pct_FIRE_I', 'Pct_Poverty', 'Pct_Unemp', 'Med_HomeValue', 'Pct_White', 'Pct_BlueCollar_O', 'Pct_Hispanic']]
 assigned_var_s = Schools[['Pct_Black', 'Pct_Two_Plus', 'Pct_SNAP', 'Pct_FIRE_I', 'Pct_Poverty']]
 assigned_var_t = Tracts[['Pct_Black', 'Pct_Two_Plus', 'Pct_SNAP', 'Pct_FIRE_I', 'Pct_Poverty']]
 
@@ -60,14 +108,23 @@ var_t = var_t.drop(columns=['index'])
 var_s = var_s.drop(columns=['index'])
 
 EDA.figures(var_c, 'Counties')
-EDA.figures(var_s, 'Schools')
-EDA.figures(var_t, 'Tracts')
+# EDA.figures(var_s, 'Schools')
+# EDA.figures(var_t, 'Tracts')
 
 EDA.Descrptives(Counties, var_c)
 print('====== School Districts =======')
-EDA.Descrptives(Schools, var_s)
+# EDA.Descrptives(Schools, var_s)
 print('======== Tracts =========')
-EDA.Descrptives(Tracts, var_t)
+# EDA.Descrptives(Tracts, var_t)
+
+#stats.pearsonr(x, y)
+
+# This will make a pearson's r correlation matrix at the County scale
+# These do not work in the spyder IDE. Please see the jupyter notebook for outputs.
+corr = assigned_var_c.corr()
+corr.style.background_gradient(cmap='coolwarm')
+
+
 
 
 # if __name__ == '__main__':
